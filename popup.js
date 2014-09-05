8f6f52ee2ae6757a3208d68ec64289cb0c733aff
Wayne <dev.wayne.ix@gmail.com>
2014-09-05 15:52:30 +0800

    update

--- popup.js
+++ popup.js
@@ -1,68 +1,63 @@
-function popup() {
+function popup(template,options) {
+
 	var default_options = {
-			winCls: 'mbox-window',
-			closeCls: 'mbox-btn-close',
-			contCls: 'mbox-content',
+			overlayCls: 'popup-overlay',
 			closable: true,
-			zIndex: 2300,
-			subWin: false,
+			zIndex: 999,
 			size: {
-				x: 350,
+				x: 'auto',
 				y: 'auto'
 			},
-			onCloseBtnClick: function() {},
-			needRepose: true
 		},
-		overlayExist = false;
+		overlayExist = false,
+		opts = $.extend(opts, default_options, options);
 
+	this.overlay = $('<div/>', {
+			'class': overlayCls
+		});
+	this.overlayExist=false;
+
+	var WinLoacal = {
+
+		returnWinHeight: function() {
+			//reference: http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
+			return window.innerHeight || document.documentElement.clientHeight;
+		},
+		returnWinWidth: function() {
+			return window.innerWidth || document.documentElement.clientWidth;
+		},
+		reposeOverlay: function() {
+			overlay.css('top', (document.documentElement.scrollTop + document.body.scrollTop));
+		},
+		resizeOverlay: function() {
+			overlay.css('height', returnWinHeight());
+		}
+
+	}
+
+	var create = function(tmpl) {
+		var win = $('<div>',{'class','popup'}),
+			close=$('<div/>',{'class','close'});
+		close.appendTo(win);
+		tmpl && tmpl.appendTo(win);
+		win.css({
+			'z-index': opts.zIndex,
+			'width': opts.width,
+			'height': opts.height
+		})
+		return win;
+	}
+
+	create(template);
 	overlay.attr("style", "top: 0px; left: 0px; width: 100%; background-color: #000; z-index: 99; zoom: 1; opacity: 0.2;filter: alpha(opacity=20);position: fixed; _position:absolute;");
 
-    function returnWinHeight() {
-        //reference: http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
-        return window.innerHeight || document.documentElement.clientHeight;
-    }
-
-    function returnWinWidth() {
-        return window.innerWidth || document.documentElement.clientWidth;
-    }
-
-    function reposeOverlay() {
-        overlay.css('top', (document.documentElement.scrollTop + document.body.scrollTop));
-    }
-
-    function resizeOverlay() {
-        overlay.css('height', returnWinHeight());
-    }
-
-    function createDialog(title, cont, btns) {
-        var ret = $.create('div');
-        var dtitle = title && $.create('div', {
-            "class": 'DialogTitle'
-        }).html(title).inject(ret);
-        var dcont = cont && $.create('div', {
-            "class": 'DialogContent'
-        }).html(cont).inject(ret);
-        var dbtns = btns && $.create('div', {
-            "class": 'DialogButtons'
-        }).inject(ret);
-        /*
-        btns = btns && D.makeArray(btns);
-
-        btns && btns.forEach(function (e) {
-        createBtn(e).inject(dbtns);
-        });*/
-
-        btns && btns.forEach(function (e) {
-            e.inject(dbtns);
-        });
-
-        return {
-            dialog: ret,
-            title: dtitle,
-            content: dcont,
-            btns: dbtns
-        };
-    }
+}
 
 
+popup.prototype.open = function() {
+	this.overlayExist?this.overlay.hide():this.overlay.show();
+}
+
+popup.prototype.close = function() {
+	this.overlayExist?this.overlay.hide():this.overlay.show();
 }
